2f091df247d1206a1c74fe24203742800543f4ec
Terariver <jared.javillo@obf.ateneo.edu>
2023-03-18 12:42:01 +0800

    a

--- Ass 11/SpeedQuery.java
+++ Ass 11/SpeedQuery.java
@@ -4,38 +4,36 @@ import org.apache.hadoop.hbase.filter.BinaryComparator;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.client.*;
 
-// Get the HBase configuration object
-Configuration config = HBaseConfiguration.create();
+public class SpeedQuery {
 
-// Create a connection to the HBase cluster
-Connection connection = ConnectionFactory.createConnection(config);
+    public static void main(String[] args) throws Exception {
+        // Get the HBase configuration object
+        Configuration config = HBaseConfiguration.create();
 
-// Create a Table object for the "driver_data" table
-Table table = connection.getTable(TableName.valueOf("driver_data"));
+        // Create an HTable object for the "driver_data" table
+        HTable table = new HTable(config, "driver_data");
 
-// Create a filter to select rows where SPEED > 30
-SingleColumnValueFilter filter = new SingleColumnValueFilter(
-    Bytes.toBytes("cf"),
-    Bytes.toBytes("speed"),
-    CompareFilter.CompareOp.GREATER,
-    new BinaryComparator(Bytes.toBytes("30"))
-);
+        // Create a filter to select rows where SPEED > 30
+        SingleColumnValueFilter filter = new SingleColumnValueFilter(
+            Bytes.toBytes("cf"),
+            Bytes.toBytes("speed"),
+            CompareFilter.CompareOp.GREATER,
+            new BinaryComparator(Bytes.toBytes("30"))
+        );
 
-// Create a scan object and add the filter to it
-Scan scan = new Scan();
-scan.setFilter(filter);
+        // Create a scan object and add the filter to it
+        Scan scan = new Scan();
+        scan.setFilter(filter);
 
-// Count the number of rows matching the filter
-long rowCount = 0;
-ResultScanner scanner = table.getScanner(scan);
-for (Result result : scanner) {
-    rowCount++;
+        // Count the number of rows matching the filter
+        long rowCount = 0;
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result result : scanner) {
+            rowCount++;
+        }
+        scanner.close();
+
+        // Print the result
+        System.out.println("Number of rows where SPEED > 30: " + rowCount);
+    }
 }
-scanner.close();
-
-// Print the result
-System.out.println("Number of rows where SPEED > 30: " + rowCount);
-
-// Close the connection and table objects
-table.close();
-connection.close();
